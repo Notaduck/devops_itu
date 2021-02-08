d6774d04af8a56852e10fb7cb7e58a405619005b
joaa17038 <ninjaleopard@outlook.com>
2021-02-08 18:44:51 +0100

    Added original views structure

--- minitwit/views.py
+++ minitwit/views.py
@@ -1,7 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("Hello World!")
 
-# Create your views here.
+def index(request):
+    # Testing
+    return HttpResponse("Hello World!")
+
+
+def beforeRequest(request):
+    pass
+
+
+def afterRequest(response):
+    pass
+
+
+def timeline(request):
+    # Insert
+    return render(request, "timeline.html")
+
+
+def publicTimeline(request):
+    pass
+
+
+def userTimeLine(request):
+    pass
+
+
+def followUser(request):
+    pass
+
+
+def unfollowUser(request):
+    pass
+
+
+def addMessage(request):
+    pass
+
+
+def login(request):
+    # Insert
+    return render(request, "login.html")
+
+
+def register(request):
+    # Insert
+    return render(request, "register.html")
+
+
+def logout(request):
+    pass

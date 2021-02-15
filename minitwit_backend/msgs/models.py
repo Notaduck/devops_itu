ffc88b8764577db225cb0ada30f86a4225b34179
einarklarlund <einar.klarlund@gmail.com>
2021-02-15 17:04:18 +0100

    created Message model

--- minitwit_backend/msgs/models.py
+++ minitwit_backend/msgs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+	message_id = models.AutoField(primary_key=True)
+	author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	text = models.CharField(max_length=280)
+	pub_date = models.DateTimeField(auto_now_add=True)
+	flagged = models.IntegerField(null=True)

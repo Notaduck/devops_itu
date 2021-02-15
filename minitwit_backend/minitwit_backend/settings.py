b9350c4136127a5a7c3941dd293e095a789cc752
MalouLandsgaard <elmelund@hotmail.dk>
2021-02-15 11:38:46 +0100

    changed from subprojects to apps, added rest framework

--- minitwit_backend/minitwit_backend/settings.py
+++ minitwit_backend/minitwit_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,6 @@ DATABASES = {
 	'HOST': os.getenv('MINITWIT_DB_HOST') or '127.0.0.1',
 	'PORT': os.getenv('MINITWIT_DB_PORT') or '5432'
     }
-}
 }
 
 

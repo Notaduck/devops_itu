e63ce1d2c9da2b9048af9d66708694ecb24c7dfa
einarklarlund <einar.klarlund@gmail.com>
2021-02-15 17:03:52 +0100

    created get_all_messages function

--- minitwit_backend/msgs/views.py
+++ minitwit_backend/msgs/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from django.http import JsonResponse
+from msgs.models import Message
+from auth.models import User
 
 # Create your views here.
+def get_all_messages(request):
+    messages = list(Message.objects.all().values())
+    return JsonResponse(messages, safe=False)

8edd913a684d90ab274763180160b94a813df9bc
Emil <emil@dichmann.info>
2021-02-20 20:00:39 +0100

    Added test case for authentication.

--- minitwit_backend/users/tests.py
+++ minitwit_backend/users/tests.py
@@ -1,22 +1,26 @@
 import base64
 from django.test import TestCase, Client
 from django.urls import reverse
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from requests.auth import HTTPBasicAuth
 from users.models import User
 
-class UserAuthenticationTestCase(TestCase):
+
+def create_user(username, email, password):
+	user = User.objects.create(username=username, email=email)
+	user.set_password(password)
+	user.save()
+
+class UserAuthenticationTestCase(APITestCase):
 	def setUp(self):
-		self.client = APIClient()
-		User.objects.create(username='test', email='test@test.test', password='test')
+		create_user('test', 'test@test.test', 'test')
 
 	def test_user_creation(self):
-		User.objects.create(username='createTest', email='createTest@test.test', password='test')
+		create_user('createTest', 'createTest@test.test', 'test')
 		self.assertTrue(User.objects.filter(username='createTest').exists())
 		createTest = User.objects.get(username='createTest')
 		self.assertEqual(createTest.username, 'createTest')
 		self.assertEqual(createTest.email, 'createTest@test.test')
-		self.assertEqual(createTest.password, 'test')
 
 	def test_user_registration(self):
 		url = reverse('register')
@@ -27,6 +31,7 @@ class UserAuthenticationTestCase(TestCase):
 		}
 		response = self.client.post(url, data=data)
 		self.assertEqual(response.status_code, 204)
+		self.assertTrue(User.objects.filter(username='RegistrationTest').exists())
 
 	def test_user_uniqueness_on_registration(self):
 		url = reverse('register')
@@ -48,3 +53,9 @@ class UserAuthenticationTestCase(TestCase):
 		data['username'] = 'uniquetest'
 		response = self.client.post(url, data=data)
 		self.assertEqual(response.status_code, 204)
+
+	def test_user_authentication(self):
+		url = reverse('getcurrentuser')
+		self.client.credentials(HTTP_AUTHORIZATION='Basic {}'.format(base64.b64encode('test:test'.encode('ascii')).decode()))
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, 200)

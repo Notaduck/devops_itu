84b5e29d68d38338747643f7de105cbc8e56fa1e
MalouLandsgaard <elmelund@hotmail.dk>
2021-02-21 16:17:58 +0100

    Initial setup for social-follow function.
    Co-authored-by: joaa17038 <ninjaleopard@outlook.com>

--- minitwit_backend/social/views.py
+++ minitwit_backend/social/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render, redirect
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from django.http import JsonResponse
+from social.models import Follower
+from users.models import User
+from django.http import HttpResponse, HttpResponseBadRequest
+from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from social.serializers import FollowerSerializer
 
 # Create your views here.
+
+class FollowView(CreateAPIView):
+	serializer_class = FollowerSerializer
+	permission_classes = (IsAuthenticated,)
+	
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		self.perform_create(serializer, user=request.auth)
+		headers = self.get_success_headers(serializer.data)
+		return Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
+
+	def perform_create(self, serializer, user, no):
+		serializer.save(user=user)
+
+class UnfollowView(CreateAPIView):
+	pass

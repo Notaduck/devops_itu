f19ffea67d63f9547d0dd3da315a12075df86e67
MalouLandsgaard <elmelund@hotmail.dk>
2021-02-21 16:09:11 +0100

    added follower model in social
    Co-authored-by: joaa17038 <ninjaleopard@outlook.com>

--- minitwit_backend/social/models.py
+++ minitwit_backend/social/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Follower(models.Model):
+    follow_id = models.AutoField(primary_key=True)
+    who = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_who')
+    whom = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_whom')

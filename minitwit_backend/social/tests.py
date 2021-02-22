f39c737069e27d2e8186dae8b3c2f8835b140d5e
MalouLandsgaard <elmelund@hotmail.dk>
2021-02-22 15:10:12 +0100

    added test for each view
    Co-authored-by: notaduck <daaa@itu.dk>

--- minitwit_backend/social/tests.py
+++ minitwit_backend/social/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
 import base64
 from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 from requests.auth import HTTPBasicAuth
 from users.models import User
+from social.models import Follower
 
 
 def create_user(username, email, password):
@@ -15,18 +15,49 @@ def create_user(username, email, password):
 	user.save()
 
 class FollowTestCase(APITestCase):
-	def setUp(self):
-		create_user('who', 'test@test.test', 'test')
-        create_user('whom', 'tf@test.test', 'test')
 
-    def test_follow(self):
-        url = reverse('follow/whom')
-        self.
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        create_user('a', 'a@a.test', 'a')
+        create_user('b', 'b@b.test', 'b')
+    
+    def test_follow_user(self):
 
-	def test_user_authentication(self):
-		url = reverse('getcurrentuser')
-		self.client.credentials(HTTP_AUTHORIZATION='Basic {}'.format(base64.b64encode('test:test'.encode('ascii')).decode()))
-		response = self.client.get(url)
-		self.assertEqual(response.status_code, 200)
+        username = 'a'
+        password = 'a'
+
+        url = f'/fllws/{username}'
+        data = {'follow': 'b'}
+
+        self.client.credentials(HTTP_AUTHORIZATION='Basic {}'.format(base64.b64encode(f'{username}:{password}'.encode('ascii')).decode()))
+        response = self.client.post(url, data=data)
+
+        
+        self.assertEqual(response.status_code, 204)
+
+        who = User.objects.get_by_natural_key(username)
+        whom = User.objects.get_by_natural_key('b')
+        
+        self.assertTrue(Follower.objects.filter(who = who, whom = whom).exists())
+
+    def test_a_unfollow_b(self):
+        username = 'a'
+        password = 'a'
+
+        url = f'/fllws/{username}'
+        data = {'follow': 'b'}
+
+        self.client.credentials(HTTP_AUTHORIZATION='Basic {}'.format(base64.b64encode(f'{username}:{password}'.encode('ascii')).decode()))
+        response = self.client.post(url, data=data)
+
+        url = f'/fllws/{username}'
+        data = {'unfollow': 'b'}
+
+        response = self.client.post(url, data=data)
+
+        
+        self.assertEqual(response.status_code, 204)
+
+        who = User.objects.get_by_natural_key(username)
+        whom = User.objects.get_by_natural_key('b')
+        
+        self.assertTrue(not Follower.objects.filter(who = who, whom = whom).exists())

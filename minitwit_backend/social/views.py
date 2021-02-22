45e085ce02237e60d6d2c51f16c361b0807789b8
MalouLandsgaard <elmelund@hotmail.dk>
2021-02-22 15:12:56 +0100

    added working create and destroy to followview
    Co-authored-by: notaduck <daaa@itu.dk>

--- minitwit_backend/social/views.py
+++ minitwit_backend/social/views.py
@@ -4,25 +4,54 @@ from django.http import JsonResponse
 from social.models import Follower
 from users.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
-from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from rest_framework.generics import RetrieveAPIView, CreateAPIView, GenericAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from social.serializers import FollowerSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
 
-class FollowView(CreateAPIView):
+class FollowView(CreateAPIView, DestroyAPIView):
 	serializer_class = FollowerSerializer
 	permission_classes = (IsAuthenticated,)
+
+	def post(self, request, username, *args, **kwargs):
+		# if username != request.user.username:
+		# 	return Response(status=status.HTTP_400_BAD_REQUEST)
+
+		if request.POST.get('follow', False):
+			who = User.objects.get_by_natural_key(username)
+			whom = User.objects.get_by_natural_key(request.POST.get('follow'))
+			if not who or not whom or who == whom:
+				print('1')
+				return Response(status=status.HTTP_400_BAD_REQUEST)
+			if Follower.objects.filter(who = who, whom = whom).exists():
+				print('2')
+				return Response(status=status.HTTP_400_BAD_REQUEST)
+			return self.create(request, username, *args, **kwargs)
+			
+
+		who = User.objects.get_by_natural_key(username)
+		whom = User.objects.get_by_natural_key(request.POST.get('unfollow'))
+		if not who or not whom or who == whom:
+			print('3')
+			return Response(status=status.HTTP_400_BAD_REQUEST)
+
+		if Follower.objects.filter(who = who, whom = whom).exists():
+			return self.destroy(request, username, *args, **kwargs)
+		print('4')
+		return Response(status=status.HTTP_400_BAD_REQUEST)
 	
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
+	def create(self, request, username, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data, context={'username': username})
 		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer, user=request.auth)
+		self.perform_create(serializer)
 		headers = self.get_success_headers(serializer.data)
 		return Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
+	
+	def destroy(self, request, username, *args, **kwargs):
+		instance = Follower.objects.get(who = User.objects.get_by_natural_key(username), whom = User.objects.get_by_natural_key(request.POST.get('unfollow')))
+		self.perform_destroy(instance)
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
-	def perform_create(self, serializer, user, no):
-		serializer.save(user=user)
-
-class UnfollowView(CreateAPIView):
-	pass

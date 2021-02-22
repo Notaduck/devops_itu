2cbfeda62db7c73d37b0a08970c98bb6b6865d4b
MalouLandsgaard <elmelund@hotmail.dk>
2021-02-22 15:09:06 +0100

    fixed create in social serializer

--- minitwit_backend/social/serializers.py
+++ minitwit_backend/social/serializers.py
@@ -1,15 +1,19 @@
 from rest_framework.serializers import Serializer
 from social.models import Follower
 from rest_framework import serializers
+from users.models import User
 
 class FollowerSerializer(Serializer): 
+    follow = serializers.CharField(source='whom')
+
     class Meta:
         model = Follower
-        fields = ('who', 'whom')
+        fields = ('whom')
     
     def create(self, validated_data): 
         follow = Follower(
-            who = validated_data['user'].user_id,
-            whom = validated_data['user'].user_id
+            who = User.objects.get_by_natural_key(self.context.pop('username')),
+            whom = User.objects.get_by_natural_key(validated_data.pop('whom'))
         )
         follow.save()
+        return follow

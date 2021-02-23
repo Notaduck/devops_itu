29254441a17db75fc49b867da2fa0f68b87c8dbb
einarklarlund <einar.klarlund@gmail.com>
2021-02-23 23:43:22 +0100

    updated serializers to work with MessagesListCreateView

--- minitwit_backend/msgs/serializers.py
+++ minitwit_backend/msgs/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework.serializers import Serializer
 from msgs.models import Message
 from rest_framework import serializers
+from users.models import User
+import datetime
 
-class MessagesSerializer(Serializer): 
+class MessagesSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Message
-        fields = ('text', 'pub_date', 'flagged')
-    
-    def create(self, validated_data): 
-		message = Message(
-            text = validated_data['text'],
-            pub_date = validated_data['pub_date'],
-            author_id = validated_data['user'].user_id,
-            flagged = validated_data['flagged']
-		)
-		message.save()
+        fields = ('text', 'author', 'pub_date', 'flagged')
 
+    def create(self, validated_data): 
+        message = Message(
+            text = validated_data['text'],
+            author = validated_data['author']
+        )
+        message.save()
+
+        return message

981d1506c455f478de42aa241844b5b29c8e37b7
einarklarlund <einar.klarlund@gmail.com>
2021-02-27 14:21:45 +0100

    changed urls to follow the format from minitwit api sim tests

---
+++ minitwit_backend/msgs/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from msgs.views import MessagesListCreateView
+
+urlpatterns = [
+	path('msgs', MessagesListCreateView.as_view(), name='all_messages'),
+	path('msgs/<str:username>', MessagesListCreateView.as_view(), name='messages_per_user'),
+	path('msgs/<str:username>', MessagesListCreateView.as_view(), name='add_message')
+]

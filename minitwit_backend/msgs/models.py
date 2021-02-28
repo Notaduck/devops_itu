ee8a4432d32b935ccbdccd042d981603ce68dff0
einarklarlund <einar.klarlund@gmail.com>
2021-02-28 13:54:56 +0100

    changed flagged field from an int to a bool

--- minitwit_backend/msgs/models.py
+++ minitwit_backend/msgs/models.py
@@ -1,10 +1,11 @@
+import json
 from django.db import models
-from auth.models import User
+from users.models import User
 
 # Create your models here.
 class Message(models.Model):
 	message_id = models.AutoField(primary_key=True)
-	author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	text = models.CharField(max_length=280)
 	pub_date = models.DateTimeField(auto_now_add=True)
-	flagged = models.IntegerField(null=True)
+	flagged = models.BooleanField(default=False)

480c380436d91733d8bc58d58fd7c8390491af33
einarklarlund <einar.klarlund@gmail.com>
2021-03-01 17:28:05 +0100

    hotfix - response for unfollows

--- minitwit_backend/social/views.py
+++ minitwit_backend/social/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from django.http import JsonResponse
 from social.models import Follower
 from users.models import User
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, QueryDict
 from rest_framework.generics import RetrieveAPIView, CreateAPIView, GenericAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from social.serializers import FollowerSerializer
@@ -12,48 +12,60 @@ from rest_framework.response import Response
 
 
 class FollowView(CreateAPIView, DestroyAPIView):
+	queryset = Follower.objects.all()
 	serializer_class = FollowerSerializer
 	permission_classes = (IsAuthenticated,)
 
 	def post(self, request, username, *args, **kwargs):
 		if not User.objects.filter(username = username).exists():
 			return Response(status=status.HTTP_400_BAD_REQUEST)
-		who = User.objects.get_by_natural_key(username)
+		# who = User.objects.get_by_natural_key(username)
+		who = User.objects.get(username=username)
 
-		if request.POST.get('follow', False):
-			whomExists = User.objects.filter(username = request.POST.get('follow')).exists()
+		if request.data.get('follow', False):
+			whomExists = User.objects.filter(username = request.data.get('follow')).exists()
 
 			if not whomExists:
 				return Response(status=status.HTTP_400_BAD_REQUEST)
 
-			whom = User.objects.get_by_natural_key(request.POST.get('follow'))
+			whom = User.objects.get_by_natural_key(request.data.get('follow'))
 
 			if Follower.objects.filter(who = who, whom = whom).exists() or who == whom:
 				return Response(status=status.HTTP_400_BAD_REQUEST)
-			return self.create(request, username, *args, **kwargs)
+			return self.create(request, username, who, whom, *args, **kwargs)
 			
-		if request.POST.get('unfollow', False):
-			whomExists = User.objects.filter(username = request.POST.get('unfollow')).exists()
+		if request.data.get('unfollow', False):
+	
+			whomExists = User.objects.filter(username = request.data.get('unfollow')).exists()
 
 			if not whomExists:
 				return Response(status=status.HTTP_400_BAD_REQUEST)
 
-			whom = User.objects.get_by_natural_key(request.POST.get('unfollow'))
+			# whom = User.objects.get_by_natural_key(request.data.get('unfollow'))
+			whom = User.objects.get(username=request.data.get('unfollow'))
 
-			if Follower.objects.filter(who = who, whom = whom).exists() or who == whom:
-				return self.destroy(request, username, *args, **kwargs)
+			if Follower.objects.filter(who = who, whom = whom).exists():
+				return self.destroy(request, username, who, whom, *args, **kwargs)
+			else:
+				return Response(status=status.HTTP_204_NO_CONTENT)
+		else: 
+			return Response(status=status.HTTP_400_BAD_REQUEST)
 		
 		return Response(status=status.HTTP_400_BAD_REQUEST)
 	
-	def create(self, request, username, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data, context={'username': username})
-		serializer.is_valid(raise_exception=True)
+	def create(self, request, username, who, whom, *args, **kwargs):
+		data = request.data.copy()
+		data['who'] = who
+		data['whom'] = whom
+		serializer = self.get_serializer(data=request.data, context={'data': data})
+		serializer.is_valid(raise_exception=False)
 		self.perform_create(serializer)
 		headers = self.get_success_headers(serializer.data)
+		
 		return Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
 	
-	def destroy(self, request, username, *args, **kwargs):
-		instance = Follower.objects.get(who = User.objects.get_by_natural_key(username), whom = User.objects.get_by_natural_key(request.POST.get('unfollow')))
+	def destroy(self, request, username, who, whom, *args, **kwargs):
+		instance = Follower.objects.get(who=who, whom=whom)
 		self.perform_destroy(instance)
 		return Response(status=status.HTTP_204_NO_CONTENT)
 

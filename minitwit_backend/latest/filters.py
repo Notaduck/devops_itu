74751206751d57164b7e210d254d36ffb8964763
einarklarlund <einar.klarlund@gmail.com>
2021-03-03 11:27:47 +0100

    hotfix - replaced as signment to params.replace with function call

--- minitwit_backend/latest/filters.py
+++ minitwit_backend/latest/filters.py
@@ -3,8 +3,8 @@ from django.conf import settings
 class LatestFilterBackend:
 	def filter_queryset(self, request, queryset, view): # only reliable function to overwrite
 		param = request.query_params.get('latest', '')
-		param.replace = ('\x00', '')
-		param.replace = (',', ' ')
+		param.replace('\x00', '')
+		param.replace(',', ' ')
 		try:
 			param = int(param)
 			settings.LATEST = param

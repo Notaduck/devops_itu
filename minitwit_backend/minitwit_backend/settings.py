95857e5fc588274d61d1491ed3da55af127cdde7
Daniel Guldberg Aaes <daaa@itu.dk>
2021-03-09 11:55:28 +0100

    Update settings.py
    Quick hotfix for returning json objects

--- minitwit_backend/minitwit_backend/settings.py
+++ minitwit_backend/minitwit_backend/settings.py
@@ -33,6 +33,17 @@ if ALLOWED_HOSTS_ENV:
 
 APPEND_SLASH = False
 
+# Rest framework options
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
+
+
 
 # Application definition
 

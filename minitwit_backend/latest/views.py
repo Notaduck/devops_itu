83122e464097baf7e59cdf61a0e0b5f4f056ab21
Daniel Guldberg Aaes <daaa@itu.dk>
2021-03-09 12:45:32 +0100

    revert

--- minitwit_backend/latest/views.py
+++ minitwit_backend/latest/views.py
@@ -4,4 +4,4 @@ from django.conf import settings
 
 @api_view(['get'])
 def latest(request):
-	return Response({'latest':settings.LATEST}, content_type='application/json')
+	return Response({'latest':settings.LATEST})

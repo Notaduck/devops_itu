75862ba7c17d147e50d66abee44e035f2d410a96
Daniel Guldberg Aaes <daaa@itu.dk>
2021-03-10 16:57:49 +0100

    clean up and look for number of records

--- minitwit_backend/minitwit_backend/middleware.py
+++ minitwit_backend/minitwit_backend/middleware.py
@@ -1,25 +1,4 @@
-# import psutil
-# from minitwit_backend.metrics import Metrics
-
-# class MetricsMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-
-#     def __call__(self, request):
-#         # Code to be executed for each request before
-#         # the view (and later middleware) are called.
-
-#         response = self.get_response(request)
-
-#         # increment number of http responses after any view is called
-#         Metrics.http_responses_total.inc()
-
-#         # record cpu load after any view is called
-#         Metrics.cpu_load_percent.set(psutil.cpu_percent())
-
-#         return response
 from latest.models import Latest
-import json
 
 class LatestMiddleware():
     def __init__(self, get_response):
@@ -32,7 +11,7 @@ class LatestMiddleware():
     def process_view(self, request, view_func, view_args, view_kwargs):
         param = request.GET.get('latest')
 
-        if Latest.objects.exists():
+        if Latest.objects.all().count() > 0:
             latest = Latest.objects.all().first()
         else:
             latest = Latest()
@@ -40,8 +19,5 @@ class LatestMiddleware():
         if param:
             latest.id = param
             latest.save()
-            print('i did it!')
-        else:
-            print('i tried my best!')
         
 

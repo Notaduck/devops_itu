efe9c845738831f0f2911a58af74af177c6535dd
notaduck <dev@guldberglab.info>
2021-03-15 13:27:09 +0100

    hopefully I fixed the test stage by now

--- minitwit_frontend/minitwit_frontend/settings.py
+++ minitwit_frontend/minitwit_frontend/settings.py
@@ -91,6 +91,7 @@ WSGI_APPLICATION = 'minitwit_frontend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+print(sys.argv)
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
@@ -99,8 +100,7 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD') or 'changeme',
         'HOST': os.getenv('POSTGRES_HOST') or '127.0.0.1',
         'PORT': os.getenv('POSTGRES_PORT') or '5432'
-    } if 'test' in sys.argv else 
-    {
+    } if 'test' in sys.argv else {
         'ENGINE':'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }

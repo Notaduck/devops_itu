268f8ddf7d356595bad03b5435ff83fae6cd5e5a
notaduck <dev@guldberglab.info>
2021-03-15 12:31:47 +0100

    imported sys and enabled prometheus

--- minitwit_frontend/minitwit_frontend/settings.py
+++ minitwit_frontend/minitwit_frontend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_prometheus',
+    'django_prometheus',
     'users',
     'timeline',
     'social',
